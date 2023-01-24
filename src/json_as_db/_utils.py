bdb3d4a06c36489673653135bd332f3e2aece16a
Joona Yoon <joonas.yoon@gmail.com>
2023-01-24 20:26:42 +0900

    refactor: move method into util module

--- src/json_as_db/_utils.py
+++ src/json_as_db/_utils.py
@@ -1,5 +1,7 @@
 import copy
 
+from typing import Any, Union, List, Tuple
+
 
 def override_dict(__dict: dict, __target: dict) -> dict:
     """override base object if value is unset
@@ -18,3 +20,17 @@ def override_dict(__dict: dict, __target: dict) -> dict:
     new_dict = copy.deepcopy(__dict)
     new_dict.update(new_target)
     return new_dict
+
+
+def from_maybe_list(value: Union[Any, List[Any]]) -> Tuple[type, List[Any]]:
+    return_type = type(value)
+    if not isinstance(value, list):
+        value = [value]
+    return (return_type, value)
+
+
+def return_maybe(_type: type, _values: List[Any]) -> Union[Any, List[Any]]:
+    if _type is list:
+        return _values
+    else:
+        return _values[0]
